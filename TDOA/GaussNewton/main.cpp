ac6bbc4595ea7888c084dfdc60105bb351201e4b
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-02-16 01:19:14 +0530

    costfunction gradient

--- TDOA/GaussNewton/main.cpp
+++ TDOA/GaussNewton/main.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <cmath>
 
-#define C 1500
+#define C 1500.0
 
 double pinger[3];
-double hydrophones[8][3];
+double hydrophone[8][3];
 
-int peaktime[8];
-int tdoa[7];
+double peaktime[8];
+double tdoa[7];
 
 double residual[7];
 
 double normdiffs[8];
 
-double jacobian[3];
+double costfunction_gradient[3];
 
 void meastdoa() {
     for (int i = 0;i <= 6;i++) {
@@ -23,9 +23,9 @@ void meastdoa() {
 
 void calcnormdiffs() {
     for (int i = 0; i <= 7; i++) {
-        double dx = pinger[0] - hydrophones[i][0];
-        double dy = pinger[1] - hydrophones[i][1];
-        double dz = pinger[2] - hydrophones[i][2];
+        double dx = pinger[0] - hydrophone[i][0];
+        double dy = pinger[1] - hydrophone[i][1];
+        double dz = pinger[2] - hydrophone[i][2];
 
         normdiffs[i] = std::sqrt(dx * dx + dy * dy + dz * dz);
     }
@@ -38,7 +38,22 @@ void calcresidual() {
 }
 
 void costfunction() {
+    costfunction_gradient[0] = 0.0;
+    costfunction_gradient[1] = 0.0;
+    costfunction_gradient[2] = 0.0;
 
+    double buffx = ((pinger[0] - hydrophone[0][0]) / normdiffs[0]);
+    double buffy = ((pinger[1] - hydrophone[0][1]) / normdiffs[0]);
+    double buffz = ((pinger[2] - hydrophone[0][2]) / normdiffs[0]);
+
+    for (int i = 0;i <= 6;i++) {
+        costfunction_gradient[0] += (-1.0 / C) * residual[i] *
+            (((pinger[0] - hydrophone[i + 1][0]) / normdiffs[i + 1]) - buffx);    //possible divide by zero???
+        costfunction_gradient[1] += (-1.0 / C) * residual[i] *
+            (((pinger[1] - hydrophone[i + 1][1]) / normdiffs[i + 1]) - buffy);
+        costfunction_gradient[2] += (-1.0 / C) * residual[i] *
+            (((pinger[2] - hydrophone[i + 1][2]) / normdiffs[i + 1]) - buffz);
+    }
 }
 
 int main() {
