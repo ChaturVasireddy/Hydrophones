5eb96554a23bc3b41d7fe1bff3e52fd9a60a178f
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-06-08 19:34:27 +0530

    printing format error fix

--- ADC/ADC.cpp
+++ ADC/ADC.cpp
@@ -69,7 +69,7 @@ void core1_entry() {
                     printf("40k\n");
                 }
                 for (int i = 0; i < N; ++i) {
-                    printf("%u\n", bin[0][i] & 0x0FFF);
+                    printf("%d\n", bin[0][i] & 0x0FFF);
                     sleep_ms(10);
                 }
                 return;
@@ -97,7 +97,7 @@ void core1_entry() {
                     printf("40k\n");
                 }
                 for (int i = 0; i < N; ++i) {
-                    printf("%u\n", bin[1][i] & 0x0FFF);
+                    printf("%d\n", bin[1][i] & 0x0FFF);
                     sleep_ms(10);
                 }
                 return;
