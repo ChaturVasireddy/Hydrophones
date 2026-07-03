005ced1e305a28aabb4da46a6cbf3824450f08d2
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-07-03 17:54:15 +0530

    master generates a clock signal with pio

--- TDOA/TDOA.cpp
+++ TDOA/TDOA.cpp
@@ -2,39 +2,17 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 
-#include "blink.pio.h"
+#include "clock.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
-
-int main()
-{
+int main() {
     stdio_init_all();
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    PIO pio;
+    uint sm;
+    uint offset;
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&clock_program, &pio,
+        &sm, &offset, 0, 1, true);
+    hard_assert(success);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    clock_program_init(pio, sm, offset, 0);
 }
